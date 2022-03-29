a72cc64a970582c67d6e87218ee69d8c82364c52
Will Wright <william.cass.wright@gmail.com>
2022-03-29 16:55:21 -0400

    conda update

--- followup-scripts/mconda.sh
+++ followup-scripts/mconda.sh
@@ -13,6 +13,27 @@ bash ~/miniconda.sh -b -p $HOME/miniconda
 rm ~/miniconda.sh
 
 source $HOME/miniconda/bin/activate
+
+### MAKE SURE THE FOLLOWING CONTENTS ARE ADDED TO `.zshrc`
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/ww931d/miniconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/ww931d/miniconda/etc/profile.d/conda.sh" ]; then
+        . "/Users/ww931d/miniconda/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/ww931d/miniconda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+###
+# ...continued:
+
 conda init zsh
 conda config --set auto_activate_base false
 
